e6a1fca9b581593a7df6cebd2939f67f9ff4d79e
hieucoding123 <phamhieu211206@gmail.com>
2024-09-21 18:52:44 +0700

    recommit

--- QuickDate/abc.cpp
+++ QuickDate/abc.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 using namespace std;
 
-char main()
+int main()
 {
-	char a;
-	cin >> a;
+	int day, month;
 
-	if ((a >= 'a') && (a <= 'z')) {
-		a -= 32;
+	cin >> day >> month;
+	switch (month) {
+	case 1:
+	case 2:
+		cout << "Spring"; break;
+	case 3:
+		if (day <= 21) cout << "Spring";
+		else cout << "Summer";
+		break;
+	case 4:
+	case 5:
+		cout << "Summer"; break;
+	case 6:
+		if (day <= 22) cout << "Summer";
+		else cout << "Autumn";
+		break;
+	case 7:
+	case 8:
+		cout << "Autumn"; break;
+	case 9:
+		if (day <= 23) cout << "Autumn";
+		else cout << "Winter";
+		break;
+	case 10:
+	case 11:
+		cout << "Winter"; break;
+	case 12:
+		if (day <= 22) cout << "Winter";
+		else cout << "Spring";
 	}
-	else if ((a >= 'A') && (a <= 'Z')) {
-		a += 32;
-	}
-	else {
-		;
-	}
-	cout << a;
 
 	return 0;
 }
