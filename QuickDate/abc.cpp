258adb1c641ccaf347e3c5e0fd163f24a388a034
hieucoding123 <phamhieu211206@gmail.com>
2024-10-07 10:59:27 +0700

    sắp xếp phần tử của 1 mảng

--- QuickDate/abc.cpp
+++ QuickDate/abc.cpp
@@ -796,3 +796,26 @@ int main()
 	cout << "Number of words: " << words.size();
 	return 0;
 }*/
+
+/*#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	cin >> n;
+	vector <int> A(n);
+	for (int i = 0; i < n; i++)
+		cin >> A[i];
+	for (int i = 0; i < n-1; i++)
+		for (int j = 0; j < n-1; j++)
+			if (A[j] > A[j + 1]) {
+				swap(A[j], A[j+1]);
+			}
+	for (int i = 0; i < n; i++)
+		cout << A[i] << ' ';
+	return 0;
+}*/
+
